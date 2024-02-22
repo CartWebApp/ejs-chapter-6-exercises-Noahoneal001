dfd7a74d7268bac54ecb5fc34e57295e718ddda4
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-02-22 15:50:28 -0800

    :(

--- 3-iterable.js
+++ 3-iterable.js
@@ -8,11 +8,69 @@ It is okay if your iterator behaves strangely when the group is modified during 
 
 // Your code here (and the code from the previous exercise)
 
+class Group {
+  // Your code here.
+  constructor(array) {
+    this.array = array;
+    
+  };
+
+  static has(number) {
+    return this.array.includes(number);
+  };
+
+  static add(adder) {
+    if (!this.array.includes(adder)) {
+      this.array.push(adder);
+    };
+  };
+
+  static delete(target) {
+    this.array = this.array.filter((word) => word != target);
+  };
+
+  static from(arraySetter) {
+    this.array = arraySetter;
+    return this;
+  };
+
+  // Symbol.iterator () {}
+
+};
+
+class GroupIterator {
+
+  constructor(Group) {
+    this.Group = Group
+    this.Iterator = 0
+  }
+
+  static next() {
+
+    console.log(this.Group)
+
+    if(this.Iterator == this.Group.array.length) {return {done: true}}
+
+    let output = this.array.array[this.Iterator]
+
+    this.Iterator++
+
+    return {value: 1, done: false}
+
+  }
+
+}
+
+Group.prototype[Symbol.iterator] = function () {return new GroupIterator(this);}
+
+console.log(Group.prototype[Symbol.iterator])
+
+console.log(Group.from([`a`,`b`,`c`]))
 
 // Tests:
 for (let value of Group.from(["a", "b", "c"])) {
-    console.log(value);
-  }
-  // → a
-  // → b
-  // → c
+  console.log(value);
+}
+// → a
+// → b
+// → c
